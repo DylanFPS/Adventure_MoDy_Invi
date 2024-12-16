e274b222a9fe818456abcdaba829e045ad6f0f2e
Moritz Salvetti <samo@it-tf.local>
2024-12-16 11:51:12 +0100

    Ich habe Die Idle Animationen Begonnen
    Ich habe die Player Idle Animationen Front erstellt, dazu habe ich Idle für rechts links und back erstellt. Und ich habe die bewegungen für den Player erstellt

---
+++ Assets/Player Script.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float richtungh = Input.GetAxis("Horizontal");
+        float richtungv = Input.GetAxis("Vertical");
+
+        if(richtungh < 0)
+        {
+            transform.Translate(Vector2.left * speed * -richtungh * Time.deltaTime);
+        }
+        if(richtungh > 0)
+        {
+            transform.Translate(Vector2.right * speed * richtungh * Time.deltaTime);
+        }
+        if(richtungv < 0)
+        {
+            transform.Translate(Vector2.down * speed * -richtungv * Time.deltaTime);
+        }
+        if(richtungv > 0)
+        {
+            transform.Translate(Vector2.up * speed * richtungv * Time.deltaTime);
+        }
+
+    }
+}
