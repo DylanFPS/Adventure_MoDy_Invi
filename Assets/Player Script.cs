fb09ef4b1a3b2fd1fc3c7cb541ab9ae2e9041fd1
Moritz Salvetti <samo@it-tf.local>
2024-12-18 15:43:35 +0100

    Ich habe die Animation des Spielers erstellt
    Ich habe die Idle- Rechts, Links, Front und Back erstellt. Dann habe ich die Animationen für das gehen nach rechts, links , vor und zurück. Dann habe ich noch die Übergänge gemacht, doch es hat noch ein Problem beim laufen gegen Links

--- Assets/Player Script.cs
+++ Assets/Player Script.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public float speed = 5;
+    private Animator Player;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,21 +20,42 @@ public class PlayerScript : MonoBehaviour
         float richtungh = Input.GetAxis("Horizontal");
         float richtungv = Input.GetAxis("Vertical");
 
+
         if(richtungh < 0)
         {
             transform.Translate(Vector2.left * speed * -richtungh * Time.deltaTime);
+            Player.SetBool("isRunningLeft", true);
+        }
+        else
+        {
+            Player.SetBool("isRunningLeft", false);
         }
         if(richtungh > 0)
         {
             transform.Translate(Vector2.right * speed * richtungh * Time.deltaTime);
+            Player.SetBool("isRunningRight", true);
+        }
+        else
+        {
+            Player.SetBool("isRunningRight", false);
         }
         if(richtungv < 0)
         {
             transform.Translate(Vector2.down * speed * -richtungv * Time.deltaTime);
+            Player.SetBool("isRunningDown", true);
+        }
+        else
+        {
+            Player.SetBool("isRunningDown", false);
         }
         if(richtungv > 0)
         {
             transform.Translate(Vector2.up * speed * richtungv * Time.deltaTime);
+            Player.SetBool("isRunningUp", true);
+        }
+        else
+        {
+            Player.SetBool("isRunningUp", false);
         }
 
     }
